fadf8e089e444af9e173f2f103b9fa5616318438
gertahoxha1 <hoxha_gerta14@hotmail.com>
2024-02-29 16:58:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// let file = "gerta.txt";
+
+// fetch (file)
+// .then(x => x.text())
+// .then(y => document.getElementById("text").innerHTML = y);
+
+
+let z = testi (4,3)
+document.getElementById('text').innerHTML = z;
+function testi(x, y){
+    return x*y;
+}
+console.log(z)
